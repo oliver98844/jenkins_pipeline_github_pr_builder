5652f30776807270514fc86749032ec6ff2ed89b
Oliver Huang <oliverhuang@kkbox.com>
2020-06-02 15:21:18 +0800

    [chore]: setup pipeline

--- pipeline-config.groovy
+++ pipeline-config.groovy
@@ -64,6 +64,8 @@ def CheckoutSCM() {
     sh '''#!/bin/bash -l
     set -ex
     echo ${sha1}
+
+    ls -l
     '''   
 }
 
@@ -83,9 +85,7 @@ def TestWithFlaky() {
 void setBuildStatus(String context, String message, String state) {
   step([
       $class: "GitHubCommitStatusSetter",
-      reposSource: [$class: "ManuallyEnteredRepositorySource", url: env.GIT_URL],
       contextSource: [$class: "ManuallyEnteredCommitContextSource", context: context],
-      errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
       statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
   ]);
 }
