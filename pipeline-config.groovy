1130786e3666b53f2e37c1350dd55b88c6c9f300
Oliver Huang <oliverhuang@kkbox.com>
2020-06-02 11:52:14 +0800

    [chore]: setup pipeline

--- pipeline-config.groovy
+++ pipeline-config.groovy
@@ -16,30 +16,32 @@ pipeline {
                 CheckoutSCM()
             }
         }
-        parallel {
-            stage('Checker1') {
-                waitUntil {
-                    setBuildStatus("checker-1", "Checker1", "PENDING")
-                    try {
-                        TestWithFlaky()
-                        setBuildStatus("checker-1", "Checker1", "SUCCESS")
-                    } catch (error) {
-                        setBuildStatus("checker-1", "Checker1", "FAILURE")
-                        input "Retry the job?"
-                        false
+        stage('Checkers') {}
+            parallel {
+                stage('Checker1') {
+                    waitUntil {
+                        setBuildStatus("checker-1", "Checker1", "PENDING")
+                        try {
+                            TestWithFlaky()
+                            setBuildStatus("checker-1", "Checker1", "SUCCESS")
+                        } catch (error) {
+                            setBuildStatus("checker-1", "Checker1", "FAILURE")
+                            input "Retry the job?"
+                            false
+                        }
                     }
                 }
-            }
-            stage('Checker2') {
-                waitUntil {
-                    setBuildStatus("checker-2", "Checker2", "PENDING")
-                    try {
-                        TestWithFlaky()
-                        setBuildStatus("checker-2", "Checker2", "SUCCESS")
-                    } catch (error) {
-                        setBuildStatus("checker-2", "Checker2", "FAILURE")
-                        input "Retry the job?"
-                        false
+                stage('Checker2') {
+                    waitUntil {
+                        setBuildStatus("checker-2", "Checker2", "PENDING")
+                        try {
+                            TestWithFlaky()
+                            setBuildStatus("checker-2", "Checker2", "SUCCESS")
+                        } catch (error) {
+                            setBuildStatus("checker-2", "Checker2", "FAILURE")
+                            input "Retry the job?"
+                            false
+                        }
                     }
                 }
             }
